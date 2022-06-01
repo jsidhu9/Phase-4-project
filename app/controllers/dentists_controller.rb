e802edaff7d5042714987c3a759486c37ca17c09
Alexandra Davis <alex.davis806@gmail.com>
2022-06-01 08:04:41 -0700

    cleaned up

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -7,44 +7,5 @@ class DentistsController < ApplicationController
     render json: dentists
   end
 
-  # # GET /dentists/1
-  # def show
-  #   render json: @dentist
-  # end
-
-  # # POST /dentists
-  # def create
-  #   @dentist = Dentist.new(dentist_params)
-
-  #   if @dentist.save
-  #     render json: @dentist, status: :created, location: @dentist
-  #   else
-  #     render json: @dentist.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # PATCH/PUT /dentists/1
-  # def update
-  #   if @dentist.update(dentist_params)
-  #     render json: @dentist
-  #   else
-  #     render json: @dentist.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # DELETE /dentists/1
-  # def destroy
-  #   @dentist.destroy
-  # end
-
-  # private
-  #   # Use callbacks to share common setup or constraints between actions.
-  #   def set_dentist
-  #     @dentist = Dentist.find(params[:id])
-  #   end
-
-  #   # Only allow a list of trusted parameters through.
-    # def dentist_params
-    #   params.fetch(:dentist, {})
-    # end
+ 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,57 +9,13 @@ class UsersController < ApplicationController
   end
 
   def me
-    # check if user is logged in if the session user ID exists 
-    # byebug
     user = User.find(session[:user_id])
     render json: user, status: :ok
 end 
 
-  # GET /users
-  # def index
-  #   users = User.all
-
-  #   render json: users
-  # end
-
-  # # GET /users/1
-  # def show
-  #   user = User.find(session[:user_id])
-  #   render json: user, status: :ok
-  # end
-
-  # # POST /users
-  # def create
-  #   @user = User.new(user_params)
-
-  #   if @user.save
-  #     render json: @user, status: :created
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
-
-  # # DELETE /users/1
-  # def destroy
-  #   @user.destroy
-  # end
+ 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
     def user_params
       params.permit(:id, :name, :password)
     end
